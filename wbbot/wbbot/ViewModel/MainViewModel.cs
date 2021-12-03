55c983fa7e186c5cca76611ade61a9c0e0704fba
MegaMozg86 <alex.tim1986@gmail.com>
2021-12-03 19:42:16 +0700

    2

--- wbbot/wbbot/ViewModel/MainViewModel.cs
+++ wbbot/wbbot/ViewModel/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Threading;
 using wbbot.Helper;
 using wbbot.View;
 using wbbot.Model;
@@ -25,6 +26,7 @@ namespace wbbot.ViewModel
         }
 
         IWebDriver driver;
+        DispatcherTimer timer;
 
         string phoneNumber;
         public string PhoneNumber
@@ -96,9 +98,65 @@ namespace wbbot.ViewModel
             }
         }
 
+        void DoIt()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            //driver.Navigate().GoToUrl("https://seller.wildberries.ru/cmp/campaigns/list/pause/edit/search/502540");
+            driver.Navigate().GoToUrl("https://seller.wildberries.ru/cmp/campaigns/list/pause/edit/search/505131");
+
+            System.Threading.Thread.Sleep(10000);
+            IWebElement query = driver.FindElement(By.ClassName("SimpleInput--vVIag"));
+            query.SendKeys(PhoneNumber);
+            IWebElement button = driver.FindElement(By.ClassName("Button--full-width--DVZvW"));
+            button.Click();
+
+            CodeViewModel codeViewModel = new CodeViewModel(driver);
+            CodeWindow codeWindow = new CodeWindow();
+            codeWindow.DataContext = codeViewModel;
+            codeWindow.ShowDialog();
+            System.Threading.Thread.Sleep(20000);
+            //driver.Navigate().GoToUrl("https://seller.wildberries.ru/cmp/campaigns/list/pause/edit/search/502540");
+            driver.Navigate().GoToUrl("https://seller.wildberries.ru/cmp/campaigns/list/pause/edit/search/505131");
+
+
+            System.Threading.Thread.Sleep(30000);
+            // Определяем позицию
+            IWebElement span = driver.FindElement(By.ClassName("card__settings__row__box--place")).FindElement(By.TagName("span"));
+            var positions = span.Text.Split('-');
+            if (positions[0] != "1")
+            {
+                // Получаем стоимость первого места
+                IWebElement money = driver.FindElement(By.ClassName("places__text"));
+                var m = money.Text.Split(' ');
+                int s = int.Parse(m[0]);
+                if (s > Limit)
+                {
+                    Stop();
+                    return;
+                }
+                // вставляем сумму в поле ввода
+                IWebElement input = driver.FindElement(By.ClassName("form__input--white"));
+                input.Clear();
+                input.SendKeys(m[0]);
+                span.Click();// делаем ставку
+
+                // нажимаем кнопку сохранить компанию
+                //          IWebElement btnSave = driver.FindElement(By.ClassName("begin-container")).FindElement(By.ClassName("m-l-24"));
+                IWebElement btnSave = driver.FindElement(By.ClassName("begin-container")).FindElement(By.ClassName("btn--outline"));
+                btnSave.Click();
+            }
+
+            // клик на кнопке статистики
+            //IWebElement link = driver.FindElement(By.ClassName("m-b-48"))
+            //.FindElement(By.ClassName("m-t-24"))
+            //.FindElement(By.ClassName("preview-link"));
+            //link.Click();
+        }
+
         void Stop()
         {
-            timer stop
+            timer.Stop();
 
             // вставляем сумму в поле ввода
             IWebElement input = driver.FindElement(By.ClassName("form__input--white"));
@@ -117,63 +175,20 @@ namespace wbbot.ViewModel
             {
                 return startCommand ??
                     (startCommand = new RelayCommand(obj =>
-                    {                        
-                        driver = new FirefoxDriver();
-                        driver.Manage().Window.Maximize();
-                        //driver.Navigate().GoToUrl("https://seller.wildberries.ru/cmp/campaigns/list/pause/edit/search/502540");
-                        driver.Navigate().GoToUrl("https://seller.wildberries.ru/cmp/campaigns/list/pause/edit/search/505131");
+                    {
+                        DoIt();
 
-                        System.Threading.Thread.Sleep(10000);
-                        IWebElement query = driver.FindElement(By.ClassName("SimpleInput--vVIag"));
-                        query.SendKeys(PhoneNumber);
-                        IWebElement button = driver.FindElement(By.ClassName("Button--full-width--DVZvW"));
-                        button.Click();
-
-                        CodeViewModel codeViewModel = new CodeViewModel(driver);
-                        CodeWindow codeWindow = new CodeWindow();
-                        codeWindow.DataContext = codeViewModel;
-                        codeWindow.ShowDialog();
-                        System.Threading.Thread.Sleep(20000);
-                        //driver.Navigate().GoToUrl("https://seller.wildberries.ru/cmp/campaigns/list/pause/edit/search/502540");
-                        driver.Navigate().GoToUrl("https://seller.wildberries.ru/cmp/campaigns/list/pause/edit/search/505131");
-                        
-                        
-                        System.Threading.Thread.Sleep(30000);
-                        // Определяем позицию
-                        IWebElement span = driver.FindElement(By.ClassName("card__settings__row__box--place")).FindElement(By.TagName("span"));
-                        var positions = span.Text.Split('-');
-                        if(positions[0] != "1")
-                        {
-                            // Получаем стоимость первого места
-                            IWebElement money = driver.FindElement(By.ClassName("places__text"));
-                            var m = money.Text.Split(' ');
-                            int s = int.Parse(m[0]);
-                            if(s > Limit)
-                            {
-                                Stop();
-                                return;
-                            }
-                            // вставляем сумму в поле ввода
-                            IWebElement input = driver.FindElement(By.ClassName("form__input--white"));
-                            input.Clear();
-                            input.SendKeys(m[0]);
-                            span.Click();// делаем ставку
-
-                            // нажимаем кнопку сохранить компанию
-                            //          IWebElement btnSave = driver.FindElement(By.ClassName("begin-container")).FindElement(By.ClassName("m-l-24"));
-                            IWebElement btnSave = driver.FindElement(By.ClassName("begin-container")).FindElement(By.ClassName("btn--outline"));
-                            btnSave.Click();
-                        }
-                                              
-                        // клик на кнопке статистики
-                        //IWebElement link = driver.FindElement(By.ClassName("m-b-48"))
-                        //.FindElement(By.ClassName("m-t-24"))
-                        //.FindElement(By.ClassName("preview-link"));
-                        //link.Click();
-
-
+                        DispatcherTimer timer = new DispatcherTimer();
+                        timer.Interval = TimeSpan.FromSeconds(Freq);
+                        timer.Tick += Timer_Tick;
+                        timer.Start();
                     }));
             }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DoIt();
         }
 
         private RelayCommand stopCommand;
